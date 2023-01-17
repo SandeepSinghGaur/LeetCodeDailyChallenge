21b7d73c7c929f517b68069bc899cfb52eb82493
Sandeep Singh <sandeep.singh@optioncircle.com>
2023-01-17 15:48:50 +0530

    Problem based on intuition Date-17/01/2023

--- JANUARY2023/Day17Jan2023.java
+++ JANUARY2023/Day17Jan2023.java
@@ -1,5 +1,29 @@
 package JANUARY2023;
 
 public class Day17Jan2023 {
+
+    public static void main(String[] args){
+        String s="010101";
+        int res = minFlipsMonoIncr(s);
+        System.out.println("Minimum number of flips required"+" "+res);
+    }
+
+    private static int minFlipsMonoIncr(String s) {
+        int m = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            if (s.charAt(i) == '0') {
+                ++m;
+            }
+        }
+        int ans = m;
+        for (int i = 0; i < s.length(); ++i) {
+            if (s.charAt(i) == '0') {
+                ans = Math.min(ans, --m);
+            } else {
+                ++m;
+            }
+        }
+        return ans;
+    }
     
 }
